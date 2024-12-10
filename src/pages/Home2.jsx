a574bfcbd1da44fced499412b96669e9b59a692e
Jake Ku <jaku910@uw.edu>
2024-12-09 16:16:24 -0800

    revert

--- src/pages/Home2.jsx
+++ src/pages/Home2.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import { useNavigate } from "react-router-dom"; // Import navigation hook
-import "./Home.css"; // Import the CSS file
 
 export default function Home() {
     const navigate = useNavigate(); // Initialize navigation
@@ -13,13 +12,6 @@ export default function Home() {
                 <h1 className="home-heading">Keepsake</h1>
                 <h2>Where Memories Become Reality</h2>
                 <div className="button-container">
-                    {/* Button to navigate to the Journal page */}
-                    <button
-                        className="navigate-button"
-                        onClick={() => navigate("/journal")}
-                    >
-                        Start Your Journal
-                    </button>
                 </div>
             </div>
         </>
